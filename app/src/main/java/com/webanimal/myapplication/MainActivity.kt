f85f3d9514e9b9dcb53012c8109f0b9005c6610a
Yuri4444 <67749511+Yuri4444@users.noreply.github.com>
2020-11-13 20:40:37 +0200

    ДЗ - 3 (13.11.2020)

--- app/src/main/java/com/webanimal/myapplication/MainActivity.kt
+++ app/src/main/java/com/webanimal/myapplication/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.webanimal.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val go : TextView = findViewById(R.id.go)
+        go.setOnClickListener {
+            var intent = Intent(this, MovieDetailsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
